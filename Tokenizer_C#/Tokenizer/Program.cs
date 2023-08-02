aa5eb07f0e37173ad6f3113c7b73536fa80d3faa
da-yamaguchi <d-yamaguchi@link-tech.jp>
2023-08-02 10:31:27 +0900

    変更した内容をコミット

--- Tokenizer_C#/Tokenizer/Program.cs
+++ Tokenizer_C#/Tokenizer/Program.cs
@@ -1,36 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.DeepDev;
+using Microsoft.Extensions.Configuration;
 
-namespace Microsoft.DeepDev
+// var c = new ConfigurationBuilder()
+//     .AddUserSecrets<Program>()
+//     .Build();
+var tokenizer = await TokenizerBuilder.CreateByModelNameAsync("gpt-3.5-turbo");
+
+Console.WriteLine("トークンを数えたい文字列を入力してください。");
+var input = Console.ReadLine()!.Trim();
+var encoded = tokenizer.Encode(input, Array.Empty<string>());
+Console.WriteLine($"{encoded.Count} tokens.");
+
+var client = new OpenAIClient(
+    // c.GetValue<Uri>("Endpoint"),
+    // new AzureKeyCredential(c.GetValue<string>("Key")!));
+    new Uri("endpointを直接"),
+    new AzureKeyCredential("keyを直接"));
+var result = await client.GetCompletionsAsync("デプロイ名を直接", new CompletionsOptions
 {
-    internal class Program
+    MaxTokens = 100,
+    Prompts =
     {
-        /// <summary>
-        /// This is a console app using the TokenizerLib to perform encoding of a string
-        /// Example usage: Tokenizer.exe "gpt-3.5-turbo" "hello, world"
-        /// </summary>
-        /// <param name="args">args[0] -- model name, args[1] -- string to be encoded</param>
-        static async Task Main(string[] args)
-        {
-            try
-            {
-                var tokenizer = await TokenizerBuilder.CreateByModelNameAsync(args[0]);
-                Console.WriteLine($"Tokenizing: [{args[1]}]");
-                var encoded = tokenizer.Encode(args[1], new List<string>());
-                for (var i = 0; i < encoded.Count; i++)
-                {
-                    var token = tokenizer.Decode(new int[] { encoded[i] });
-                    Console.WriteLine($"{token}, {encoded[i]}");
-                }
-                var decoded = tokenizer.Decode(encoded.ToArray());
-                Console.WriteLine($"Decoded: [{decoded}]");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Example usage: Tokenizer.exe \"gpt-3.5-turbo\" \"hello, world\"");
-                Console.WriteLine($"Error running tokenizer:\n {ex.ToString()}");
-            }
-        }
+        input,
     }
-}
+});
+
+Console.WriteLine($"{result.Value.Usage.PromptTokens} tokens.");
+
